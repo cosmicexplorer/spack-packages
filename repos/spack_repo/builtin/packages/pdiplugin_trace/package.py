feff17f944e091474bd85dcc05d12284cf7fb71e
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2026-03-22 14:19:15 -0400

    this makes other things compile. goodness

--- repos/spack_repo/builtin/packages/pdiplugin_trace/package.py
+++ repos/spack_repo/builtin/packages/pdiplugin_trace/package.py
@@ -21,8 +21,47 @@ class PdipluginTrace(CMakePackage):
 
     maintainers("jbigot")
 
-    for v in Pdi.versions:
-        version(str(v), **Pdi.versions[v])
+
+    # only the latest version is supported upstream
+    # we also offer the last 2 patch versions of the current minor
+    # and the last patch version of the previous 2 minors
+    # all the rest is marked as deprecated
+    version("develop", branch="main", no_cache=True)
+    version("1.10.1", sha256="f2409611310255360867e3c74d44e055f45c87071ef8881665ff30a7940e1449")
+    version("1.10.0", sha256="8bda1ed83bdb152a047a45a48f896466e7ebf5163030405c15dbfa4e2e788143")
+    version("1.9.3", sha256="ab390e51e3b7298d6b09484a443dc267651aed5978c711f8804848b19ab1527e")
+    version(
+        "1.9.2",
+        sha256="0430d5898980435e5602b67188264621a27f71969ff886efaa2e6d43a45caac4",
+        deprecated=True,
+    )
+    version(
+        "1.9.1-fixed",
+        sha256="13d052a7d5d53271638382f06e9da0d58b01ed9cfdf9c4fa1e82367b9e1732e1",
+        deprecated=True,
+    )
+    version(
+        "1.9.0",
+        sha256="04fee7851c4f2a156daddf7eb2c3c3b0132d80d3f0e448cdeebda0b7c4595639",
+        deprecated=True,
+    )
+    version("1.8.3", sha256="df7200289a2a368ec874140039b417abdfe681b57fb1b9f4c52f924952226020")
+    version(
+        "1.8.2",
+        sha256="bb4d1654c97f7ff379067adbff339f8b4117c0cf9432f41f1a5cb20a747cac1a",
+        deprecated=True,
+    )
+    version(
+        "1.8.1",
+        sha256="43f0c0b2bda5515ecf99da7be1600af2c1f669d6c73e3f309275b14940c7e35c",
+        deprecated=True,
+    )
+    version(
+        "1.8.0",
+        sha256="5d353bfa64f45ee4715b88bd30330030f79f2020cd6bede0ad9b8f9beddadea9",
+        deprecated=True,
+    )
+
 
     variant("tests", default=False, description="Build tests")
 
@@ -31,8 +70,19 @@ class PdipluginTrace(CMakePackage):
 
     depends_on("cmake@3.22.1:", type=("build"), when="@1.10.0:")
     depends_on("cmake@3.16.3:", type=("build"))
-    for v in Pdi.versions:
-        depends_on("pdi@" + str(v), type=("link", "run"), when="@" + str(v))
+
+
+    depends_on("pdi@1.10.1", type=("link", "run"), when="@1.10.1")
+    depends_on("pdi@1.10.0", type=("link", "run"), when="@1.10.0")
+    depends_on("pdi@1.9.3", type=("link", "run"), when="@1.9.3")
+    depends_on("pdi@1.9.2", type=("link", "run"), when="@1.9.2")
+    depends_on("pdi@1.9.1-fixed", type=("link", "run"), when="@1.9.1-fixed")
+    depends_on("pdi@1.9.0", type=("link", "run"), when="@1.9.0")
+    depends_on("pdi@1.8.3", type=("link", "run"), when="@1.8.3")
+    depends_on("pdi@1.8.2", type=("link", "run"), when="@1.8.2")
+    depends_on("pdi@1.8.1", type=("link", "run"), when="@1.8.1")
+    depends_on("pdi@1.8.0", type=("link", "run"), when="@1.8.0")
+
     depends_on("pkgconfig", type=("build"))
 
     root_cmakelists_dir = "plugins/trace"
